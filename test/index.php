d9f544776a8f2a7fd2d3073805a78c5303fe7d4b
Mitar <mitar.git@tnode.com>
2023-07-11 02:39:08 +0200

    Test PHP.

--- test/index.php
+++ test/index.php
@@ -2,9 +2,12 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Test site</title>
+    <title><?php echo "Test site" ?></title>
   </head>
   <body>
     <p>Test site</p>
   </body>
 </html>
+<?php
+error_log("logging an error in PHP", 0);
+?>
